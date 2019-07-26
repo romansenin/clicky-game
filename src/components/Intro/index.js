dca4c5a2d28454c0d6112d950e1ce131d4ea676b
Roman <romansenin15@gmail.com>
2019-07-25 19:20:33 -0700

    added Intro component

---
+++ src/components/Intro/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Intro() {
+  return (
+    <div className="container">
+      <h1>Clicky Game!</h1>
+      <h3>Click on an image to earn points, but don't click on any more than once!</h3>
+      <a href="#">Get Started</a>
+    </div>
+  )
+}
+
+export default Intro;
