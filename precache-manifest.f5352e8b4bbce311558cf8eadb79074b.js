083d4fc729ffd73aebcd97c54676636f2b15e10e
Roman <romansenin15@gmail.com>
2019-07-29 17:34:57 -0700

    Updates

---
+++ precache-manifest.f5352e8b4bbce311558cf8eadb79074b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3903b0506969b02bd86cf2bc009962",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "532f219c21f48197cf21",
+    "url": "/clicky-game/static/css/main.ec9cb050.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/clicky-game/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "532f219c21f48197cf21",
+    "url": "/clicky-game/static/js/main.142fa98f.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
