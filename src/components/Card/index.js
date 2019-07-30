56bc35663f283ebe20afff6137bb6bd4b4c14b8f
Roman <romansenin15@gmail.com>
2019-07-30 12:52:11 -0700

    disabled draggable attribute

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -10,7 +10,7 @@ export default class Card extends Component {
   render() {
     return (
       <div className="card">
-        <img src={this.props.image.image} alt="" />
+        <img draggable="false" src={this.props.image.image} alt="" />
       </div>
     );
   }
