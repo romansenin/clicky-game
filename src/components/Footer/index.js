06f5bf8c3094295651e316183759838d2a71505c
Roman <romansenin15@gmail.com>
2019-07-31 16:08:17 -0700

    added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./footer.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="footer-container">
+        <p>&copy; 2019</p>
+      </div>
+    );
+  }
+}
